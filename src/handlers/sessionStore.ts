b4a537eca5f609bd2935547d49adab07d3385d9d
achul123 <thetheatregaming@gmail.com>
2025-12-08 16:21:54 +0100

    update years

--- src/handlers/sessionStore.ts
+++ src/handlers/sessionStore.ts
@@ -3,7 +3,7 @@
  *      AirLink - Open Source Project by AirlinkLabs
  *      Repository: https://github.com/airlinklabs/panel
  *
- *     © 2024 AirlinkLabs. Licensed under the MIT License
+ *     © 2025 AirlinkLabs. Licensed under the MIT License
  * ╳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╳
  */
 
