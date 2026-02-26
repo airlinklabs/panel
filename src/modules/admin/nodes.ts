a1b197aa457f906457978bded103d0875b743247
Thavanish brijesh <thavanishbrijesh@gmail.com>
2026-02-26 15:57:48 +0530

    Change default port

--- src/modules/admin/nodes.ts
+++ src/modules/admin/nodes.ts
@@ -374,7 +374,7 @@ const adminModule: Module = {
           const address = req.body.address;
           const port = parseInt(req.body.port);
           const allocatedPorts = req.body.allocatedPorts || '[]';
-          const sftpPort = parseInt(req.body.sftpPort) || 22;
+          const sftpPort = parseInt(req.body.sftpPort) || 3003;
 
           if (
             !name ||
